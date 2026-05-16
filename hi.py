9eec2edfd892fc28fa9425b475f15e28a37f9faf
Bhuwan Bhatt <ogbhuwanbhatt@gmail.com>
2026-05-16 20:48:31 +0545

    opencv

--- hi.py
+++ hi.py
@@ -1,1 +1,9 @@
-print("Hello, Guys!")
+("Hello, Guys!")
+import cv2
+import cvzone
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
